533e50225a384d281f8bc3886fc5b2dee03b91ba
Alexandru Placinta <alexandru.placinta@metaco.com>
2024-08-02 22:11:50 +0300

    Boot prints to VGA

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,20 @@ use core::panic::PanicInfo;
 
 #[panic_handler]
 fn panic(_panic_info: &PanicInfo) -> ! {
-    loop {
-
-    }
+    loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    loop {
+    static HELLO: &[u8] = b"Hello World!";
+    let vga_buffer = 0xb8000 as *mut u8;
 
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
     }
+
+    loop {}
 }
