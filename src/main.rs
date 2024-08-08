e8fbd3e28d5d3835f74a3990f2408fb01bec58d9
Alexandru Placinta <alexandru.placinta@metaco.com>
2024-08-08 16:19:32 +0300

    VGA test mode

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,19 @@
 #![no_std]
 #![no_main]
 
+mod vga_buffer;
+
 use core::panic::PanicInfo;
 
 #[panic_handler]
-fn panic(_panic_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    static HELLO: &[u8] = b"Hello World!";
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
+    println!("Hello world{}", "!");
+    panic!("Kernel panic");
     loop {}
 }
